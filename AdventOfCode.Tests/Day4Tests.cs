d3858fd3ae5800c3d93b4f48a70209deedfe0317
Neil Cross <neil@tigerspike.com>
2015-12-15 17:38:09 +1100

    Complete Day 4

---
+++ AdventOfCode.Tests/Day4Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode.Tests
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Day4Tests
+    {
+        private Day4 sut;
+
+        private string puzzleInput;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.sut = new Day4();
+            
+            this.puzzleInput = "bgvyzdsv";
+        }
+
+        [Test]
+        public void Part1Examples()
+        {
+            Assert.AreEqual(609043, sut.FindHash("abcdef"));
+            Assert.AreEqual(1048970, sut.FindHash("pqrstuv"));
+        }
+
+        [Test]
+        public void Part1Result()
+        {
+            var result = sut.FindHash(this.puzzleInput);
+            Trace.WriteLine("Result is " + result);
+        }
+
+        [Test]
+        public void Part2Result()
+        {
+            var result = sut.FindHash(this.puzzleInput, "000000");
+            Trace.WriteLine("Result is " + result);
+        }
+    }
+}
---
+++ AdventOfCode/Day4.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class Day4
+    {
+        private MD5 hasher;
+
+        public Day4()
+        {
+        }
+
+        public int FindHash(string key, string requiredPrefix = "00000")
+        {
+            using (this.hasher = MD5.Create())
+            {
+                key += "{0}";
+                int iCount = -1;
+                do
+                {
+                    iCount++;
+                }
+                while (!this.HashBeginsWith(string.Format(key, iCount),requiredPrefix));
+
+                return iCount;
+            }
+        }
+        
+
+        private bool HashBeginsWith(string key, string prefix)
+        {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = this.hasher.ComputeHash(Encoding.UTF8.GetBytes(key));
+            
+            var stringBuilder = new StringBuilder();
+
+            var iterationRequired = Math.Ceiling(prefix.Length / 2f);
+
+            for (int i = 0; i < iterationRequired; i++)
+            {
+                stringBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return stringBuilder.ToString().StartsWith(prefix);
+        }
+    }
+}
